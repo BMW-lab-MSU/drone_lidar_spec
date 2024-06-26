1c3cdf95f0648615dbb8e11458c2f6c1386ca47e
zcrennen <z3crennen@gmail.com>
2024-06-26 08:26:34 -0600

    finished dataset config file

--- configs/dataset_config.py
+++ configs/dataset_config.py
@@ -1,7 +1,6 @@
 dataset_type = 'CocoDataset'
 data_root = 'specs/split_specs/'
 
-# File paths for training, validation, and testing datasets
 train_ann_file = data_root + 'train/annotations.json'
 train_img_prefix = data_root + 'train/Raw/'
 
@@ -11,54 +10,46 @@ val_img_prefix = data_root + 'val/Raw/'
 test_ann_file = data_root + 'test/annotations.json'
 test_img_prefix = data_root + 'test/Raw/'
 
-# Normalization values
-# UPDATED THESE WITH CALCULATED VALUES
-norm_mean = [123.675, 116.28, 103.53]
-norm_std = [58.395, 57.12, 57.375]
+normalization_values = {
+    'mean': [123.675, 116.28, 103.53],
+    'std': [58.395, 57.12, 57.375]
+}
 
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
+    samples_per_gpu=2, #How many images per batch
+    workers_per_gpu=2, #Number of CPU workers to load data for each GPU
     train=dict(
         type=dataset_type,
-        ann_file=train_ann_file,  # Train annotations file
-        img_prefix=train_img_prefix,  # Raw images for training
+        ann_file=train_ann_file,
+        img_prefix=train_img_prefix,
         pipeline=[
             dict(type='LoadImageFromFile'),
             dict(type='LoadAnnotations', with_bbox=True),
-            dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-            dict(type='Normalize', mean=norm_mean, std=norm_std, to_rgb=True),
-            dict(type='Pad', size_divisor=32),
-            dict(type='DefaultFormatBundle'),
-            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+            dict(type='Normalize', **normalization_values, to_rgb=True),
+            dict(type='DefaultFormatBundle'), #Processes annotations (such as converting to tensor)
+            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']), #Prepares data to feed into modellll
         ]
     ),
     val=dict(
         type=dataset_type,
-        ann_file=val_ann_file,  # Validation annotations file
-        img_prefix=val_img_prefix,  # Raw images for validation
+        ann_file=val_ann_file,
+        img_prefix=val_img_prefix,
         pipeline=[
             dict(type='LoadImageFromFile'),
-            dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-            dict(type='Normalize', mean=norm_mean, std=norm_std, to_rgb=True),
-            dict(type='Pad', size_divisor=32),
+            dict(type='Normalize', **normalization_values, to_rgb=True),
             dict(type='DefaultFormatBundle'),
             dict(type='Collect', keys=['img']),
         ]
     ),
     test=dict(
         type=dataset_type,
-        ann_file=test_ann_file,  # Test annotations file
-        img_prefix=test_img_prefix,  # Raw images for testing
+        ann_file=test_ann_file,
+        img_prefix=test_img_prefix,
         pipeline=[
             dict(type='LoadImageFromFile'),
-            dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-            dict(type='Normalize', mean=norm_mean, std=norm_std, to_rgb=True),
-            dict(type='Pad', size_divisor=32),
+            dict(type='Normalize', **normalization_values, to_rgb=True),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
         ]
     )
 )
-
-evaluation = dict(interval=1, metric='bbox')
