edf8c17650e3ebc224e14a093b57bfb12b3e1103
zcrennen <z3crennen@gmail.com>
2024-06-27 09:24:55 -0600

    changed model config to inherit from base model

--- configs/model_config.py
+++ configs/model_config.py
@@ -1,5 +1,8 @@
+_base_ = [
+    './mmdetection/configs/_base_/models/faster_rcnn_r50_fpn.py'
+]
+
 model = dict(
-    type='FasterRCNN',
     backbone=dict(
         type='ResNeXt',
         depth=101,
@@ -7,66 +10,9 @@ model = dict(
         base_width=4,
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnext101_32x4d')
     ),
-    neck=dict(
-        type='FPN',
-        in_channels=[256, 512, 1024, 2048],
-        out_channels=256,
-        num_outs=5
-    ),
-    rpn_head=dict(
-        type='RPNHead',
-        in_channels=256,
-        feat_channels=256,
-        anchor_generator=dict(
-            type='AnchorGenerator',
-            scales=[8],
-            ratios=[0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
-            strides=[4, 8, 16, 32, 64]
-        ),
-        loss_cls=dict(
-            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0
-        ),
-        loss_bbox=dict(type='L1Loss', loss_weight=1.0)
-    ),
     roi_head=dict(
-        type='StandardRoIHead',
-        bbox_roi_extractor=dict(
-            type='SingleRoIExtractor',
-            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=2),
-            out_channels=256,
-            featmap_strides=[4, 8, 16, 32]
-        ),
         bbox_head=dict(
-            type='Shared4Conv1FCBBoxHead',
-            in_channels=256,
-            fc_out_channels=1024,
-            roi_feat_size=7,
-            num_classes=1,
-            bbox_coder=dict(
-                type='DeltaXYWHBBoxCoder',
-                target_means=[0., 0., 0., 0.],
-                target_stds=[0.1, 0.1, 0.2, 0.2]
-            ),
-            reg_class_agnostic=False,
-            loss_cls=dict(
-                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0
-            ),
-            loss_bbox=dict(type='L1Loss', loss_weight=1.0)
-        )
-    ),
-    test_cfg=dict(
-        rpn=dict(
-            nms_across_levels=False,
-            nms_pre=2000,
-            nms_post=1000,
-            max_num=1000,
-            nms_thr=0.7,
-            min_bbox_size=0
-        ),
-        rcnn=dict(
-            score_thr=0.05,
-            nms=dict(type='nms', iou_threshold=0.5),
-            max_per_img=1  # Only keep the highest scoring box per image
+            num_classes=1,  # Adjust the number of classes as needed
         )
     )
 )
