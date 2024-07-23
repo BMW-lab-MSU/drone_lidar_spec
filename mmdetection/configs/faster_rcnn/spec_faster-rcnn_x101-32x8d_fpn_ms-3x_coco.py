9a47d4b5d99218df21d77f4664da10ebeca3c18e
zcrennen <z3crennen@gmail.com>
2024-07-23 10:18:27 -0600

    added support for only one scale and stride combo

--- mmdetection/configs/faster_rcnn/spec_faster-rcnn_x101-32x8d_fpn_ms-3x_coco.py
+++ mmdetection/configs/faster_rcnn/spec_faster-rcnn_x101-32x8d_fpn_ms-3x_coco.py
@@ -1,7 +1,5 @@
 _base_ = ['../common/ms_3x_coco.py', '../_base_/models/faster-rcnn_r50_fpn.py']
 model = dict(
-    # ResNeXt-101-32x8d model trained with Caffe2 at FB,
-    # so the mean and std need to be changed.
     data_preprocessor=dict(
         type='DetDataPreprocessor',
         mean=[103.530, 116.280, 123.675],
@@ -14,10 +12,52 @@ model = dict(
         groups=32,
         base_width=8,
         num_stages=4,
-        out_indices=(0, 1, 2, 3),
+        out_indices=(2,),  # Use only the third stage feature map
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=False),
         style='pytorch',
         init_cfg=dict(
             type='Pretrained',
-            checkpoint='/home/d86p233/Desktop/BMW-spec/mmdetection/checkpoints/faster_rcnn_x101_32x8d_fpn_mstrain_3x_coco_20210604_182954-002e082a.pth')))
+            checkpoint='/home/d86p233/Desktop/BMW-spec/mmdetection/checkpoints/faster_rcnn_x101_32x8d_fpn_mstrain_3x_coco_20210604_182954-002e082a.pth')),
+    neck=dict(
+        type='FPN',
+        in_channels=[1024],  # Only the third stage feature map
+        out_channels=256,
+        num_outs=1),  # Only one output from FPN
+    rpn_head=dict(
+        type='RPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[5],  # Single scale
+            ratios=[31],  # Ratio for the desired anchor size
+            strides=[5]),  # Single stride
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+    roi_head=dict(
+        type='StandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[5]),  # Single stride
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=80,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0.0, 0.0, 0.0, 0.0],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0))))
