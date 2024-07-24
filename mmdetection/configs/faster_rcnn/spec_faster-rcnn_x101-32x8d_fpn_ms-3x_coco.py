8a837bfe79c021ff9781bd1e5526877dc9ce509c
zcrennen <z3crennen@gmail.com>
2024-07-24 11:20:16 -0600

    updated new config to use identical format as old config, with only changes to the model

--- mmdetection/configs/faster_rcnn/spec_faster-rcnn_x101-32x8d_fpn_ms-3x_coco.py
+++ mmdetection/configs/faster_rcnn/spec_faster-rcnn_x101-32x8d_fpn_ms-3x_coco.py
@@ -1,5 +1,77 @@
 _base_ = ['../common/ms_3x_coco.py', '../_base_/models/faster-rcnn_r50_fpn.py']
 
+dataset_type = 'CocoDataset'
+classes = ('drone_frequency', )
+
+normalization_values = {
+    'mean': [128.41, 205.31, 76.05],
+    'std': [52.56, 22.05, 31.79]
+}
+
+# DataLoader settings for training
+train_dataloader = dict(
+    batch_size=8,
+    num_workers=2,
+    dataset=dict(
+        type=dataset_type,
+        metainfo=dict(classes=classes),
+        data_root='placeholder',  # Placeholder to be replaced by cfg-options
+        ann_file='placeholder/train/annotations.json',  # Placeholder to be replaced by cfg-options
+        data_prefix=dict(img='placeholder/train/'),  # Placeholder to be replaced by cfg-options
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(type='Resize', scale=(775, 462)),
+            dict(type='Normalize', **normalization_values, to_rgb=True),
+            dict(type='PackDetInputs')
+        ]
+    )
+)
+
+# DataLoader settings for validation
+val_dataloader = dict(
+    batch_size=8,
+    num_workers=2,
+    dataset=dict(
+        type=dataset_type,
+        metainfo=dict(classes=classes),
+        data_root='placeholder',  # Placeholder to be replaced by cfg-options
+        ann_file='placeholder/val/annotations.json',  # Placeholder to be replaced by cfg-options
+        data_prefix=dict(img='placeholder/val/'),  # Placeholder to be replaced by cfg-options
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(type='Resize', scale=(775, 462)),
+            dict(type='Normalize', **normalization_values, to_rgb=True),
+            dict(type='PackDetInputs')
+        ]
+    )
+)
+
+# DataLoader settings for testing
+test_dataloader = dict(
+    batch_size=8,
+    num_workers=2,
+    dataset=dict(
+        type=dataset_type,
+        metainfo=dict(classes=classes),
+        data_root='placeholder',  # Placeholder to be replaced by cfg-options
+        ann_file='placeholder/test/annotations.json',  # Placeholder to be replaced by cfg-options
+        data_prefix=dict(img='placeholder/test/'),  # Placeholder to be replaced by cfg-options
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(type='Resize', scale=(775, 462)),
+            dict(type='Normalize', **normalization_values, to_rgb=True),
+            dict(type='PackDetInputs')
+        ]
+    )
+)
+
+# Evaluator settings
+val_evaluator = dict(ann_file='placeholder/val/annotations.json')  # Placeholder to be replaced by cfg-options
+test_evaluator = dict(ann_file='placeholder/test/annotations.json')  # Placeholder to be replaced by cfg-options
+
 model = dict(
     data_preprocessor=dict(
         type='DetDataPreprocessor',
@@ -63,128 +135,43 @@ model = dict(
                 type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
             loss_bbox=dict(type='L1Loss', loss_weight=1.0))))
 
-optim_wrapper = dict(
-    optimizer=dict(lr=0.02, momentum=0.9, type='SGD', weight_decay=0.0001),
-    type='OptimWrapper')
-
-param_scheduler = [
-    dict(
-        begin=0, by_epoch=False, end=500, start_factor=0.001, type='LinearLR'),
-    dict(
-        begin=0,
-        by_epoch=True,
-        end=12,
-        gamma=0.1,
-        milestones=[9, 11],
-        type='MultiStepLR'),
-]
-
-resume = False
-
-test_cfg = dict(type='TestLoop')
-
-test_dataloader = dict(
-    batch_size=1,
-    dataset=dict(
-        ann_file='annotations/instances_val2017.json',
-        backend_args=None,
-        data_prefix=dict(img='val2017/'),
-        data_root='data/coco/',
-        pipeline=[
-            dict(backend_args=None, type='LoadImageFromFile'),
-            dict(type='LoadAnnotations', with_bbox=True),
-            dict(
-                meta_keys=(
-                    'img_id',
-                    'img_path',
-                    'ori_shape',
-                    'img_shape',
-                    'scale_factor',
-                ),
-                type='PackDetInputs'),
-        ],
-        test_mode=True,
-        type='CocoDataset'),
-    drop_last=False,
-    num_workers=2,
-    persistent_workers=True,
-    sampler=dict(shuffle=False, type='DefaultSampler'))
-
-test_evaluator = dict(
-    ann_file='data/coco/annotations/instances_val2017.json',
-    backend_args=None,
-    metric='bbox',
-    type='CocoMetric')
-
-train_cfg = dict(max_epochs=12, type='EpochBasedTrainLoop', val_interval=1)
-
-train_dataloader = dict(
-    batch_sampler=dict(type='AspectRatioBatchSampler'),
-    batch_size=2,
-    dataset=dict(
-        dataset=dict(
-            ann_file='annotations/instances_train2017.json',
-            backend_args=None,
-            data_prefix=dict(img='train2017/'),
-            data_root='data/coco/',
-            filter_cfg=dict(filter_empty_gt=True, min_size=32),
-            pipeline=[
-                dict(backend_args=None, type='LoadImageFromFile'),
-                dict(type='LoadAnnotations', with_bbox=True),
-                dict(type='PackDetInputs'),
-            ],
-            type='CocoDataset'),
-        times=3,
-        type='RepeatDataset'),
-    num_workers=2,
-    persistent_workers=True,
-    sampler=dict(shuffle=True, type='DefaultSampler'))
-
-train_pipeline = [
-    dict(backend_args=None, type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='PackDetInputs'),
-]
-
-val_cfg = dict(type='ValLoop')
-
-val_dataloader = dict(
-    batch_size=1,
-    dataset=dict(
-        ann_file='annotations/instances_val2017.json',
-        backend_args=None,
-        data_prefix=dict(img='val2017/'),
-        data_root='data/coco/',
-        pipeline=[
-            dict(backend_args=None, type='LoadImageFromFile'),
-            dict(type='LoadAnnotations', with_bbox=True),
-            dict(
-                meta_keys=(
-                    'img_id',
-                    'img_path',
-                    'ori_shape',
-                    'img_shape',
-                    'scale_factor',
-                ),
-                type='PackDetInputs'),
-        ],
-        test_mode=True,
-        type='CocoDataset'),
-    drop_last=False,
-    num_workers=2,
-    persistent_workers=True,
-    sampler=dict(shuffle=False, type='DefaultSampler'))
-
-val_evaluator = dict(
-    ann_file='data/coco/annotations/instances_val2017.json',
-    backend_args=None,
-    metric='bbox',
-    type='CocoMetric')
-
-vis_backends = [dict(type='LocalVisBackend')]
-
-visualizer = dict(
-    name='visualizer',
-    type='DetLocalVisualizer',
-    vis_backends=[dict(type='LocalVisBackend')],
+# Optimizer settings
+optimizer = dict(
+    type='SGD',
+    lr=0.01,
+    momentum=0.9,
+    weight_decay=0.0001,
+    paramwise_cfg=dict(
+        custom_keys={'backbone': dict(lr_mult=0.1)}
+    )
 )
+
+# Learning rate configuration without warmup
+lr_config = dict(
+    policy='step',
+    step=[8, 11],
+    gamma=0.1
+)
+
+# Checkpoint settings
+checkpoint_config = dict(
+    interval=1
+)
+
+# Logging settings
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ]
+)
+
+# Evaluation settings
+evaluation = dict(
+    interval=1,
+    metric='bbox'
+)
+
+# Workflow settings
+workflow = [('train', 1), ('val', 1)]
