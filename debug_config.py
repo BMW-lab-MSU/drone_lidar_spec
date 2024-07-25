02e68639b2f6b8e5586d553182404257ba9ddc70
zcrennen <z3crennen@gmail.com>
2024-07-25 09:21:59 -0600

    added debug_config

--- debug_config.py
+++ debug_config.py
@@ -1,7 +1,8 @@
 auto_scale_lr = dict(base_batch_size=16, enable=False)
 backend_args = None
-data_root = 'data/noboost_prop-only-90/'
+data_root = '/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90'
 dataset_type = 'CocoDataset'
+classes = ('drone_frequency', )
 default_hooks = dict(
     checkpoint=dict(interval=1, type='CheckpointHook'),
     logger=dict(interval=50, type='LoggerHook'),
@@ -15,7 +16,7 @@ env_cfg = dict(
     cudnn_benchmark=False,
     dist_cfg=dict(backend='nccl'),
     mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
-launcher = 'slurm'
+launcher = 'none'
 load_from = None
 log_level = 'INFO'
 log_processor = dict(by_epoch=True, type='LogProcessor', window_size=50)
@@ -26,59 +27,30 @@ model = dict(
         frozen_stages=1,
         groups=32,
         init_cfg=dict(
-            checkpoint=
-            '/home/d86p233/Desktop/BMW-spec/mmdetection/checkpoints/resnext101_32x8d-1516f1aa.pth',
+            checkpoint='/home/d86p233/Desktop/BMW-spec/mmdetection/checkpoints/resnext101_32x8d-1516f1aa.pth',
             type='Pretrained'),
         norm_cfg=dict(requires_grad=False, type='BN'),
         norm_eval=True,
         num_stages=4,
-        out_indices=(
-            0,
-            1,
-            2,
-            3,
-        ),
+        out_indices=(0, 1, 2, 3),
         style='pytorch',
         type='ResNeXt'),
     data_preprocessor=dict(
         bgr_to_rgb=False,
-        mean=[
-            117.72,
-            201.1,
-            82.63,
-        ],
+        mean=[117.72, 201.1, 82.63],
         pad_size_divisor=32,
-        std=[
-            55.09,
-            24.4,
-            33.32,
-        ],
+        std=[55.09, 24.4, 33.32],
         type='DetDataPreprocessor'),
     neck=dict(
-        in_channels=[
-            256,
-            512,
-            1024,
-            2048,
-        ],
+        in_channels=[256, 512, 1024, 2048],
         num_outs=5,
         out_channels=256,
         type='FPN'),
     roi_head=dict(
         bbox_head=dict(
             bbox_coder=dict(
-                target_means=[
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                ],
-                target_stds=[
-                    0.1,
-                    0.1,
-                    0.2,
-                    0.2,
-                ],
+                target_means=[0.0, 0.0, 0.0, 0.0],
+                target_stds=[0.1, 0.1, 0.2, 0.2],
                 type='DeltaXYWHBBoxCoder'),
             fc_out_channels=1024,
             in_channels=256,
@@ -90,47 +62,20 @@ model = dict(
             roi_feat_size=7,
             type='Shared2FCBBoxHead'),
         bbox_roi_extractor=dict(
-            featmap_strides=[
-                4,
-                8,
-                16,
-                32,
-            ],
+            featmap_strides=[4, 8, 16, 32],
             out_channels=256,
             roi_layer=dict(output_size=7, sampling_ratio=0, type='RoIAlign'),
             type='SingleRoIExtractor'),
         type='StandardRoIHead'),
     rpn_head=dict(
         anchor_generator=dict(
-            ratios=[
-                0.5,
-                1.0,
-                2.0,
-            ],
-            scales=[
-                8,
-            ],
-            strides=[
-                4,
-                8,
-                16,
-                32,
-                64,
-            ],
+            ratios=[0.5, 1.0, 2.0],
+            scales=[8],
+            strides=[4, 8, 16, 32, 64],
             type='AnchorGenerator'),
         bbox_coder=dict(
-            target_means=[
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-            ],
-            target_stds=[
-                1.0,
-                1.0,
-                1.0,
-                1.0,
-            ],
+            target_means=[0.0, 0.0, 0.0, 0.0],
+            target_stds=[1.0, 1.0, 1.0, 1.0],
             type='DeltaXYWHBBoxCoder'),
         feat_channels=256,
         in_channels=256,
@@ -189,15 +134,8 @@ model = dict(
             nms_pre=2000)),
     type='FasterRCNN')
 normalization_values = dict(
-    mean=[
-        117.72,
-        201.1,
-        82.63,
-    ], std=[
-        55.09,
-        24.4,
-        33.32,
-    ])
+    mean=[117.72, 201.1, 82.63],
+    std=[55.09, 24.4, 33.32])
 optim_wrapper = dict(
     optimizer=dict(lr=0.02, momentum=0.9, type='SGD', weight_decay=0.0001),
     type='OptimWrapper')
@@ -209,10 +147,7 @@ param_scheduler = [
         by_epoch=True,
         end=12,
         gamma=0.1,
-        milestones=[
-            9,
-            11,
-        ],
+        milestones=[9, 11],
         type='MultiStepLR'),
 ]
 resume = False
@@ -220,26 +155,15 @@ test_cfg = dict(type='TestLoop')
 test_dataloader = dict(
     batch_size=1,
     dataset=dict(
-        ann_file='data/noboost_prop-only-90/test/annotations/annotations.json',
+        ann_file='/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90/test/annotations/annotations.json',
         backend_args=None,
-        data_prefix=dict(img='data/noboost_prop-only-90/test/images/'),
-        data_root='data/noboost_prop-only-90/',
+        data_prefix=dict(
+            img='/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90/test/images/'),
+        data_root='/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90',
         pipeline=[
             dict(backend_args=None, type='LoadImageFromFile'),
-            dict(keep_ratio=True, scale=(
-                1333,
-                800,
-            ), type='Resize'),
             dict(type='LoadAnnotations', with_bbox=True),
-            dict(
-                meta_keys=(
-                    'img_id',
-                    'img_path',
-                    'ori_shape',
-                    'img_shape',
-                    'scale_factor',
-                ),
-                type='PackDetInputs'),
+            dict(type='PackDetInputs'),
         ],
         test_mode=True,
         type='CocoDataset'),
@@ -248,25 +172,16 @@ test_dataloader = dict(
     persistent_workers=True,
     sampler=dict(shuffle=False, type='DefaultSampler'))
 test_evaluator = dict(
-    ann_file='data/noboost_prop-only-90/test/annotations/annotations.json',
+    ann_file='/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90/test/annotations/annotations.json',
     backend_args=None,
     metric='bbox',
     type='CocoMetric')
 test_pipeline = [
     dict(backend_args=None, type='LoadImageFromFile'),
-    dict(keep_ratio=True, scale=(
-        1333,
-        800,
-    ), type='Resize'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(
         meta_keys=(
-            'img_id',
-            'img_path',
-            'ori_shape',
-            'img_shape',
-            'scale_factor',
-        ),
+            'img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'),
         type='PackDetInputs'),
 ]
 train_cfg = dict(max_epochs=12, type='EpochBasedTrainLoop', val_interval=1)
@@ -274,81 +189,42 @@ train_dataloader = dict(
     batch_sampler=dict(type='AspectRatioBatchSampler'),
     batch_size=2,
     dataset=dict(
-        dataset=dict(
-            ann_file=
-            'data/noboost_prop-only-90/train/annotations/annotations.json',
-            backend_args=None,
-            data_prefix=dict(img='data/noboost_prop-only-90/train/images/'),
-            data_root='data/noboost_prop-only-90/',
-            filter_cfg=dict(filter_empty_gt=True, min_size=32),
-            pipeline=[
-                dict(backend_args=None, type='LoadImageFromFile'),
-                dict(type='LoadAnnotations', with_bbox=True),
-                dict(
-                    keep_ratio=True,
-                    scale=[
-                        (
-                            1333,
-                            640,
-                        ),
-                        (
-                            1333,
-                            800,
-                        ),
-                    ],
-                    type='RandomResize'),
-                dict(prob=0.5, type='RandomFlip'),
-                dict(type='PackDetInputs'),
-            ],
-            type='CocoDataset'),
-        times=3,
-        type='RepeatDataset'),
+        ann_file='/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90/train/annotations/annotations.json',
+        backend_args=None,
+        data_prefix=dict(
+            img='/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90/train/images/'),
+        data_root='/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90',
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=[
+            dict(backend_args=None, type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(type='Resize', scale=(775, 462)),  # Ensure consistent size
+            dict(type='PackDetInputs'),
+        ],
+        type='CocoDataset'),
     num_workers=2,
     persistent_workers=True,
     sampler=dict(shuffle=True, type='DefaultSampler'))
 train_pipeline = [
     dict(backend_args=None, type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        keep_ratio=True,
-        scale=[
-            (
-                1333,
-                640,
-            ),
-            (
-                1333,
-                800,
-            ),
-        ],
-        type='RandomResize'),
-    dict(prob=0.5, type='RandomFlip'),
+    dict(type='Resize', scale=(775, 462)),  # Ensure consistent size
     dict(type='PackDetInputs'),
 ]
 val_cfg = dict(type='ValLoop')
 val_dataloader = dict(
     batch_size=1,
     dataset=dict(
-        ann_file='data/noboost_prop-only-90/val/annotations/annotations.json',
+        ann_file='/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90/val/annotations/annotations.json',
         backend_args=None,
-        data_prefix=dict(img='data/noboost_prop-only-90/val/images/'),
-        data_root='data/noboost_prop-only-90/',
+        data_prefix=dict(
+            img='/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90/val/images/'),
+        data_root='/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90',
         pipeline=[
             dict(backend_args=None, type='LoadImageFromFile'),
-            dict(keep_ratio=True, scale=(
-                1333,
-                800,
-            ), type='Resize'),
             dict(type='LoadAnnotations', with_bbox=True),
-            dict(
-                meta_keys=(
-                    'img_id',
-                    'img_path',
-                    'ori_shape',
-                    'img_shape',
-                    'scale_factor',
-                ),
-                type='PackDetInputs'),
+            dict(type='Resize', scale=(775, 462)),  # Ensure consistent size
+            dict(type='PackDetInputs'),
         ],
         test_mode=True,
         type='CocoDataset'),
@@ -357,17 +233,13 @@ val_dataloader = dict(
     persistent_workers=True,
     sampler=dict(shuffle=False, type='DefaultSampler'))
 val_evaluator = dict(
-    ann_file='data/noboost_prop-only-90/val/annotations/annotations.json',
+    ann_file='/home/d86p233/Desktop/BMW-spec/data/noboost_prop-only-90/val/annotations/annotations.json',
     backend_args=None,
     metric='bbox',
     type='CocoMetric')
-vis_backends = [
-    dict(type='LocalVisBackend'),
-]
+vis_backends = [dict(type='LocalVisBackend')]
 visualizer = dict(
     name='visualizer',
     type='DetLocalVisualizer',
-    vis_backends=[
-        dict(type='LocalVisBackend'),
-    ])
-work_dir = 'compare/updated'
+    vis_backends=[dict(type='LocalVisBackend')])
+work_dir = '/home/d86p233/Desktop/BMW-spec/work_dirs/train_task_0'
