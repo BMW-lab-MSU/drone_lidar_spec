1d44df940ca5538badd5be58bc30c0db37216c4f
zcrennen <z3crennen@gmail.com>
2024-07-26 10:13:20 -0600

    added pipeline shell script

---
+++ sliding_window/SCC.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Variables
+IMAGES_PATH="../data/noboost_prop-only-90/val/images"
+OUTPUT_PATH="noboost_prop-only-90-val.json"
+H5_JSON="SFh5s.json"
+IMAGE_JSON="noboost_prop-only-90-val.json"
+COMPARISON_OUTPUT="noboost_prop-only-90-val-comparisons.json"
+METRICS_JSON="noboost_prop-only-90-val-comparisons.json"
+
+# Run the sliding window script
+python slide_windows.py --images_path "$IMAGES_PATH" --output_path "$OUTPUT_PATH"
+
+# Run the frequency comparison script
+python compare_frequencies.py --h5_json "$H5_JSON" --image_json "$IMAGE_JSON" --output_path "$COMPARISON_OUTPUT"
+
+# Run the evaluation metrics script
+python calc_eval_metrics.py --json_path "$METRICS_JSON"
